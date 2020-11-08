77c1f7edcadc6e358f53e89058902559a254f2c9
stianst <stianst@gmail.com>
2020-11-08 09:07:25 +0100

    CLI example for chapter 6

---
+++ ch6/app.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var open = require('open');
+var axios = require('axios');
+var querystring = require('querystring');
+
+var app = express();
+app.use(express.static('callback'));
+
+var server = app.listen(0);
+var port = server.address().port;
+
+console.info('Listening on port: ' + port + '\n');
+
+app.get('/callback/', function(req, res) {
+    res.send('<html><script>setTimeout(function() { window.close(); }, 2000);</script><body>Callback endpoint, closing in 2 seconds</body></html>');
+    var code = req.query.code;
+    server.close();
+
+    console.info('Authorization Code: ' + code + '\n');
+
+    axios.post('http://localhost:8080/auth/realms/myrealm/protocol/openid-connect/token', querystring.stringify({
+        client_id: 'cli',
+        grant_type: 'authorization_code',
+        redirect_uri: 'http://localhost:' + port + '/callback',
+        code: code
+    })).then(res => {
+        console.log('Access Token: ' + res.data.access_token + '\n');
+    }).catch(error => {
+        console.error(error);
+    });
+});
+
+open('http://localhost:8080/auth/realms/myrealm/protocol/openid-connect/auth?client_id=cli&redirect_uri=http://localhost:' + port + '/callback&response_type=code');
