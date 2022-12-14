e1ff2844bf9c74bd54520e531c817be9b1daa853
Giuseppe Sforza <mail@gsforza.de>
2022-12-14 17:25:40 +0100

    Fix URL for use with newer Keycloak versions

--- ch2/frontend/app.js
+++ ch2/frontend/app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var stringReplace = require('string-replace-middleware');
 
-var KC_URL = process.env.KC_URL || "http://localhost:8080/auth";
+var KC_URL = process.env.KC_URL || "http://localhost:8080";
 var SERVICE_URL = process.env.SERVICE_URL || "http://localhost:3000/secured";
 
 app.use(stringReplace({
